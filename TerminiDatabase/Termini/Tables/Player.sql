a2ad561a52dc702c3c4507b236a250f29441a6e8
agles <agles@gmail.com>
2025-02-25 12:36:04 +0100

    Adding database
    Add database connection and schema for Termini project
    
    Updated appsettings files to include a new ConnectionStrings section for SQL Server. Added a new project, TerminiDatabase, to the solution for managing database schema and deployment. Created a SQL project file and a schema comparison file for database management. Defined a new SQL schema, Termini, and added a Player table with necessary columns and constraints.

---
+++ TerminiDatabase/Termini/Tables/Player.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [Termini].[Player] (
+    [Id]          INT      IDENTITY (1, 1) NOT NULL,
+    [Active]      BIT      CONSTRAINT [DF_Termini_Player_Active] DEFAULT ((1)) NOT NULL,
+    [DateCreated] DATETIME CONSTRAINT [DF_Termini_Player_DateCreated] DEFAULT (getdate()) NOT NULL,
+    CONSTRAINT [PK_Termini_Player_Id] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
